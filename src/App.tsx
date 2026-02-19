c140f691437e0098b2374e6e9bb4dad458f5be6b
Bryan Flores <bryanfloresmolina08@gmail.com>
2026-02-19 11:45:06 -0400

    set inter as a font

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<h1 className="text-3xl text-blue-800 font-medium">Hello SmartOps!</h1>} />
+      <Route path="/" element={<h1 className="text-3xl text-blue-800 font-bold">Hello SmartOps!</h1>} />
     </Routes>
   )
 }
