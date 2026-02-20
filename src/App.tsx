6da10971179f2ddee02c0494780176d2da914852
Bryan Flores <bryanfloresmolina08@gmail.com>
2026-02-19 21:52:10 -0400

    supabase connection done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { Route, Routes } from "react-router-dom"
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-
-  return (
-    <Routes>
-      <Route path="/" element={<h1 className="text-3xl text-blue-800 font-bold">Hello SmartOps!</h1>} />
-    </Routes>
-  )
+  return <AppRoutes />;
 }
 
-export default App
+export default App;
