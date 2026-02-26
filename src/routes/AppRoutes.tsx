d8066ca6877cfcbecb38f3ef46aab91d9c1e1744
BryanDevop <pocoyop422@gmail.com>
2026-02-25 20:56:02 -0400

    feat(auth): implementar contexto de autenticación con Supabase y rutas protegidas
    - Se agregó AuthProvider con listener onAuthStateChange
    - Se centralizó el manejo de sesión con Supabase
    - Se implementó ProtectedRoute para rutas privadas
    - Se implementó PublicRoute para evitar acceso si ya está autenticado
    - Se agregó funcionalidad de logout en Dashboard
    - Se eliminó manejo manual de localStorage

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,14 +1,63 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "../screens/auth/Login";
-import Landing from "../screens/landing";
+import Landing from "../screens/Landing";
 import Register from "../screens/auth/Register";
+import PublicRoute from "./PublicRoute";
+import Dashboard from "../screens/Dashboard/Dashboard";
+import ProtectedRoute from "./ProtectedRoute";
+
+
+  /**
+ * En esta pantalla se trabaja de la siguiente manera:
+ * 
+ * PublicRoute::: Cualquier persona puede acceder aun sin estar logueado
+ * 
+ * ----Para poder usarlo simplemente en route dentro de element envolverlo como PublicRoute
+ * ----Y paserle como children el elemento que deseamos ponerlo en publico
+ * 
+ * ProtectRoute::: Solo podran acceder las personas que se encuentra logueada 
+ * 
+ * sin intentar acceder a una de esta ruta no podran entrar y lo envia al login en auto
+ * Para poder Usarla es de la siguente manera Waaa very easier eeh
+ * 
+ * ----Dentro de routes pasarle la ruta que estara protegida 
+ * ----Envolve el elemento dentro de ProctedRouter
+ */
 
 export default function AppRoutes() {
+
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+
+      {/* Rutas públicas controladas */}
+      <Route
+        path="/login"
+        element={
+          <PublicRoute>
+            <Login />
+          </PublicRoute>
+        }
+      />
+
+      <Route
+        path="/register"
+        element={
+          <PublicRoute>
+            <Register />
+          </PublicRoute>
+        }
+      />
+
+      {/* Ruta protegida */}
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
