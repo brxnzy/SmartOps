399733105077a6b66d311ddb6b53a8521a0700e8
DevGum <yenselmanuel0@gmail.com>
2026-02-26 08:56:20 -0400

    sidebar layout hecho

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { Route, Routes } from "react-router-dom"
-import Landing from "./screens/landing"
+import { Route, Routes } from "react-router-dom";
+import Sidebar from "./layouts/Sidebar";
+import Landing from "./screens/landing";
 
 function App() {
-
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
+      <Route path="/sidebar" element={<Sidebar />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
