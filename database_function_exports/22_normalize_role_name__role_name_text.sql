f7c2f06f8efb42ab855d0b8838515f8d47351113
Bryan Flores <bryanfloresmolina08@gmail.com>
2026-04-22 16:37:25 -0400

    archivos innecesarios removidos

--- database_function_exports/22_normalize_role_name__role_name_text.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION public.normalize_role_name(role_name text)
- RETURNS text
- LANGUAGE sql
- IMMUTABLE
-AS $function$
-  select regexp_replace(lower(coalesce(role_name, '')), '[\\s_-]+', '', 'g');
-$function$
-
